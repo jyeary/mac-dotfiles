e82c757fb67d298e70d85c0290158ed99208c90e
John Yeary <jyeary@bluelotussoftware.com>
2020-05-20 16:26:48 -0400

    Added additional aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,1 +1,11 @@
 alias weather="curl -4 http://wttr.in"
+
+# Recursively remove .DS_Store files
+alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
+
+# IP Addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
