a2a746b78a95d0cb01f34c0b9dd5319ea4e1df84
John Yeary <jyeary@bluelotussoftware.com>
2020-05-20 18:57:13 -0400

    Added additional aliases

--- aliases.zsh
+++ aliases.zsh
@@ -9,3 +9,11 @@ alias localip="ipconfig getifaddr en0"
 
 # Print each PATH entry on a separate line
 alias path='echo -e ${PATH//:/\\n}'
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
